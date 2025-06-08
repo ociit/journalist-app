b54c158fbcafcfddb2f52dc30419ee652b3623c7
ociit <141004544+ociit@users.noreply.github.com>
2025-06-08 22:02:57 +0700

    Delete news_journalist directory

--- news_journalist/android/app/src/main/kotlin/com/example/news_journalist/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.news_journalist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- news_journalist/lib/api/api_keys.dart
+++
@@ -1,6 +0,0 @@
-// lib/api/api_keys.dart
-
-class ApiKeys {
-  static const String newsApiKey = "e6495c929ad041b9876d4f68136ca370";
-  static const String weatherApiKey = "fba9d07e6d804bd6b9e73453252605 ";
-}
